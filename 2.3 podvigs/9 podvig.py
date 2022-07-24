dc03884ce13dcbe0cb7baf2f58aa8b28a9540bda
RudovichNazar23 <n.rudovich2005@gmail.com>
2022-07-24 14:39:18 +0300

    added new podvigs

---
+++ 2.3 podvigs/9 podvig.py
@@ -0,0 +1,45 @@
+class Bag:
+
+    def __init__(self, max_weight):
+        self.__things = list()
+        self.max_weight = max_weight
+
+    @property
+    def things(self):
+        return self.__things
+
+    def add_thing(self, thing):
+        if thing.weight < self.max_weight:
+            self.__things.append(thing)
+        elif thing.weight > self.max_weight
+            raise ValueError("cannot append thing, because extra weight !")
+
+    def remove_thing(self, indx):
+        self.__things.pop(indx)
+
+    def get_total_weight(self):
+        sum_of_products = int()
+        for product in self.__things:
+            sum_of_products += product.weight
+        return sum_of_products
+
+
+class Thing:
+    def __init__(self, name, weight):
+        if type(name) == str:
+            self.name = name
+        if type(weight) in (int, float):
+            self.weight = weight
+
+
+bag = Bag(1000)
+bag.add_thing(Thing("Книга по Python", 100))
+bag.add_thing(Thing("Котелок", 500))
+bag.add_thing(Thing("Спички", 20))
+bag.add_thing(Thing("Бумага", 100))
+
+w = bag.get_total_weight()
+print(w)
+
+for t in bag.things:
+    print(f"{t.name}: {t.weight}")
