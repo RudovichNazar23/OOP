03907401b512d59b59ec95265eadd0d01a5041f9
RudovichNazar23 <n.rudovich2005@gmail.com>
2022-07-29 17:33:06 +0300

    new podvigs

--- 3.1 podvigs/6 podvig.py
+++ 3.1 podvigs/6 podvig.py
@@ -4,53 +4,17 @@ class Picture:
         self.author = author
         self.descr = descr
 
-    def __setattr__(self, key, value):
-        attrs = {
-            "name": isinstance(value, str),
-            "author": isinstance(value, str),
-            "descr": isinstance(value, str)
-        }
-
-        if attrs[key]:
-            return object.__setattr__(self, key, value)
-        else:
-            raise TypeError("Неверный тип присваиваемых данных.")
-
 class Mummies:
     def __init__(self, name, location, descr):
         self.name = name
         self.location = location
         self.descr = descr
 
-    def __setattr__(self, key, value):
-        attrs = {
-            "name": isinstance(value, str),
-            "location": isinstance(value, str),
-            "descr": isinstance(value, str)
-        }
-
-        if attrs[key]:
-            return object.__setattr__(self, key, value)
-        else:
-            raise TypeError("Неверный тип присваиваемых данных.")
-
 class Papyri:
     def __init__(self, name, date, descr):
         self.name = name
         self.date = date
         self.descr = descr
-
-    def __setattr__(self, key, value):
-        attrs = {
-            "name": isinstance(value, str),
-            "date": isinstance(value, str),
-            "descr": isinstance(value, str)
-        }
-
-        if attrs[key]:
-            return object.__setattr__(self, key, value)
-        else:
-            raise TypeError("Неверный тип присваиваемых данных.")
 
 class Museum:
     def __init__(self, name):
@@ -61,16 +25,23 @@ class Museum:
         self.exhibits.append(obj.__dict__)
 
     def remove_exhibit(self, obj):
-        self.exhibits.remove(obj)
+        if obj in self.exhibits:
+            self.exhibits.remove(obj)
 
     def get_info_exhibit(self, indx):
-        for i in self.exhibits:
-            if self.exhibits.index(i) == indx:
-                return f"{i.__dict__[i.name]} : {i.__dict__[i.desc]}"
+        ex = self.exhibits[indx]
+        print(f"Описание экспоната {ex.name}: {ex.descr}")
 
 picture = Picture("Ja", "Ja", "Moja hujnia")
+mummy = Mummies("Nazar", "Egypt", "New , fresh, yummy")
+
+
 mus = Museum("Mienia")
+
 mus.add_exhibit(picture)
+mus.add_exhibit(mummy)
+
 print(mus.__dict__)
 print()
-print(mus.get_info_exhibit(0))
+
+mus.get_info_exhibit(0)
